64be16b3bfe5b41dc6ef41005cc364ecae0f297d
carlosadrr <40671490+carlosadrr@users.noreply.github.com>
2021-05-29 14:07:22 +0100

    Update data-migration.sql
    adding transaction

--- data-migration.sql
+++ data-migration.sql
@@ -1,2 +1,9 @@
+/*This script should only be used when you are using linked servers, this wont work with always on*/
+
+BEGIN TRANSACTION
 insert into CustomersTarget
 	select * from CustomersSource
+	
+	
+	
+COMMIT TRANSACTION
